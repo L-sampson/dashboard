e77eefa97c2cca59777295d9177bd38d0bb30294
L-sampson <lavontech@gmail.com>
2025-01-01 16:42:35 -0500

    Added enviroment, and connected to container backend

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Laptop, Desktop, Misc } from '../interfaces/models';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,14 @@ export class InventoryService {
   constructor(private http: HttpClient) { }
 
   getLaptops(): Observable<Laptop[]> {
-    return this.http.get<Laptop[]>('assets/laptops.json');
+    return this.http.get<Laptop[]>(`${environment.apiUrl}/laptops`);
   }
 
   getDesktops(): Observable<Desktop[]> {
-    return this.http.get<Desktop[]>('assets/desktops.json');
+    return this.http.get<Desktop[]>(`${environment.apiUrl}/desktops`);
   }
 
   getMiscItems(): Observable<Misc[]> {
-    return this.http.get<Misc[]>('assets/misc.json');
+    return this.http.get<Misc[]>(`${environment.apiUrl}/misc`);
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://py-docker.calmgrass-42b8fa4e.eastus.azurecontainerapps.io',
+};
